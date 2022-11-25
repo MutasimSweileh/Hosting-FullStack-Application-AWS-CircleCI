80be3d6ef36d6cfe7f662d0f05307b62d3b00919
MutasimSweileh <mohtasmsawilh1@gmail.com>
2022-11-25 22:10:55 +0200

    aws data

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
 # Deploy to AWS EB
 # cd ./www
-eb init myprojectv3 --region us-east-1
-eb use Myprojectv3-env
-eb deploy Myprojectv3-env
+eb init Udagram --region us-east-1
+eb use Udagram-env
+eb deploy Udagram-env
